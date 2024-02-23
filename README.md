6df1660609d40f5e70e492bb37f7f35a2e99556a
my-tricks <silva.jeremie93@gmail.com>
2024-02-23 16:18:51 +0100

    DEV

---
+++ README.md
@@ -0,0 +1,10 @@
+pipenv install
+pipenv shell
+pipenv run python3.12 app.py 
+
+
+
+sudo apt install pass
+pass init [public username]
+pass insert [word]
+$(pass ldap)
---
+++ app.py
@@ -0,0 +1,15 @@
+from argparse import ArgumentParser, Namespace
+import getpass
+
+parser = ArgumentParser(description="Prompt a username and password for authentication.")
+parser.add_argument("--username", required=True, help="User's username")
+parser.add_argument("--password", required=True, help="User's username")
+args: Namespace = parser.parse_args()
+
+username: str = args.username
+password: str = args.password
+
+
+if __name__ == "__main__":
+    print(username)
+    print(password)
